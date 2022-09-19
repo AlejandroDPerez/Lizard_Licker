18d661bb6d4a742cba377b379cc3a95e90ea4faa
MatOldham <moldham02@gmail.com>
2022-09-19 18:43:30 -0500

    Added working upgrades and a display for the licks per second.

--- Assets/GlobalLicks.cs
+++ Assets/GlobalLicks.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
+
 
 public class GlobalLicks : MonoBehaviour
 {
-    public static int lickCount;
+    public static float lickCount;
+    public static float licksPerSecond;
+    
     public GameObject lickDisplay;
-    public int internalLick;
+
+
+    private int internalLick;
+    private float updateTime;
+
+    void Start (){
+        licksPerSecond = 0.0f;
+        lickCount = 0.0f;
+    }
+
     void Update (){
-        internalLick = lickCount;
+        updateTime += Time.deltaTime;
+
+        //every tenth second
+    
+        if(updateTime > 0.1f){
+            updateTime = 0.0f;
+            lickCount += (licksPerSecond / 10.0f);
+        }
+
+        internalLick = (int) Math.Floor(lickCount);
         lickDisplay.GetComponent<TMP_Text>().text = "Licks: " + internalLick;
+        
 
     }
 }
--- Assets/LickCounter.cs
+++ Assets/LickCounter.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class LickCounter : MonoBehaviour
 {
 
 
     public void onButtonClick(){
-        GlobalLicks.lickCount += 1;
+        GlobalLicks.lickCount += (GlobalUpgrades.lickMultiplier);
+
 
     
     }
